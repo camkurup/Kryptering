57b9390c53d706c04590f877cd6c225d652e11f0
camkurup <camkurup@hotmail.com>
2022-11-11 19:35:18 +0100

    Created websocket server and client

--- Kryptering/Client.cs
+++ Kryptering/Client.cs
@@ -1,6 +1,30 @@
-﻿namespace Kryptering
+﻿using System.Net.WebSockets;
+using System.Text;
+
+namespace Kryptering
 {
     public class Client
     {
+        public async Task CreateClientAsync()
+        {
+            Console.Title = "Client";
+            using (var ws = new ClientWebSocket())
+            {
+                await ws.ConnectAsync(new Uri("ws://localhost:6666/ws"), CancellationToken.None);
+                var buffer = new byte[256];
+                while (ws.State == WebSocketState.Open)
+                {
+                    var result = await ws.ReceiveAsync(buffer, CancellationToken.None);
+                    if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
+                    }
+                    else
+                    {
+                        Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, result.Count));
+                    }
+                }
+            }
+        }
     }
 }
