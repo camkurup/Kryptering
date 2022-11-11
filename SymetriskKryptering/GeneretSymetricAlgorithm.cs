522c8a2c88954e19a0c66bb81116f37c01f1e6b8
camkurup <camkurup@hotmail.com>
2022-11-11 16:00:44 +0100

    Symetric kryptation algorithm and encrypter/decrypter added to the project

---
+++ SymetriskKryptering/GeneretSymetricAlgorithm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymetriskKryptering
+{
+    internal class GeneretSymetricAlgorithm
+    {
+        SymmetricAlgorithm mySymetricAlg;
+        public void Generate(string cipher)
+        {
+            switch (cipher)
+            {
+                case "DES":
+                    mySymetricAlg = DES.Create();
+                    break;
+                case "3DES":
+                    mySymetricAlg = TripleDES.Create();
+                    break;
+                case "Rijndael":
+                    mySymetricAlg = Rijndael.Create();
+                    break;
+            }
+            mySymetricAlg.GenerateIV();
+            mySymetricAlg.GenerateKey();
+        }
+        public byte[] Encrypt(byte[] mess, byte[] key, byte[] iv)
+        {
+            mySymetricAlg.Key = key;
+            mySymetricAlg.IV = iv;
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms,
+                mySymetricAlg.CreateEncryptor(),
+                CryptoStreamMode.Write);
+            cs.Write(mess,0, mess.Length);
+            cs.Close();
+            return ms.ToArray();
+        }
+        public byte[] Decrypt(byte[] mess, byte[] key, byte[] iv)
+        {
+            byte[] plaintext = new byte[mess.Length];
+            mySymetricAlg.Key = key;
+            mySymetricAlg.IV = iv;
+            MemoryStream ms = new MemoryStream(mess);
+            CryptoStream cs = new CryptoStream(ms,
+                mySymetricAlg.CreateDecryptor(),
+                CryptoStreamMode.Read);
+            cs.Read(plaintext, 0, mess.Length);
+            cs.Close();
+            return plaintext;
+        }
+    }
+}
