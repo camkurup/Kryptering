49ab027a19fe9a2ddb177272fedf447316e8adfe
camkurup <camkurup@hotmail.com>
2022-11-12 14:47:37 +0100

    Created the database, so its ready to take un the sleted and hashed passwords.. Besides that I have added my plan for what to do and som small code for hashing. But it does not compile at the moment

---
+++ SecurePassword/AddPasswordToDB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurePassword
+{
+    //Step 1: Generate randome long salt
+    //step 2: Add this to the password
+    //Step 3: Hash both salt and code
+    //Step 4: store in DB
+
+    internal class AddPasswordToDB
+    {
+        public static byte[] ComputeHash(string password, byte[] salt, int iterations = HasingIterationsCount, 
+            int hashByteSize = HashByteSize)
+        {
+            using (Rfc2898DeriveBytes hashGenerator = new Rfc2898DeriveBytes(password, salt))
+            {
+                hashGenerator.IterationCount = iterations;
+                return hashGenerator.GetBytes(hashByteSize);
+            }
+        }
+    }
+}
